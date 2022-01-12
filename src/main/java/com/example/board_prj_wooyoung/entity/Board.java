cd1db0537be28ea1fc2f26f5f1baf8d69a218d9a
wooyoung <souljit2@gmail.com>
2022-01-12 20:11:22 +0900

    feat: Board Entity 추가

---
+++ src/main/java/com/example/board_prj_wooyoung/entity/Board.java
@@ -0,0 +1,29 @@
+package com.example.board_prj_wooyoung.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+}
