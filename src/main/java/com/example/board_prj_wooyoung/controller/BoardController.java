3d91e8bb63c0d9c2032d205587cd43c7658a52b2
wooyoung <souljit2@gmail.com>
2022-01-13 00:14:43 +0900

    feat: 게시글 등록 API 및 화면 구현

--- src/main/java/com/example/board_prj_wooyoung/controller/BoardController.java
+++ src/main/java/com/example/board_prj_wooyoung/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.example.board_prj_wooyoung.controller;
 
+import com.example.board_prj_wooyoung.dto.BoardDTO;
 import com.example.board_prj_wooyoung.dto.PageRequestDTO;
 import com.example.board_prj_wooyoung.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -22,5 +25,24 @@ public class BoardController {
         log.info("list................." + pageRequestDTO);
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get....");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes) {
+
+        log.info("dto..." + dto);
+
+        Long bno = boardService.register(dto);
+
+        log.info("BNO: " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
     }
 }
