0c16790822cb871db54c70a52246d526e0c35d47
wooyoung <souljit2@gmail.com>
2022-01-12 19:44:05 +0900

    feat: BaseEntity 생성 및 @EnableJpaAuditing 추가

--- src/main/java/com/example/board_prj_wooyoung/BoardPrjWooyoungApplication.java
+++ src/main/java/com/example/board_prj_wooyoung/BoardPrjWooyoungApplication.java
@@ -2,8 +2,10 @@ package com.example.board_prj_wooyoung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardPrjWooyoungApplication {
 
 	public static void main(String[] args) {
