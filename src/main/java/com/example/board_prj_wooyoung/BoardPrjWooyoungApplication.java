b06cc39dc5ce8ab144fad512db887566e76a43c0
wooyoung <souljit2@gmail.com>
2022-01-12 18:57:55 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/example/board_prj_wooyoung/BoardPrjWooyoungApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_prj_wooyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPrjWooyoungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPrjWooyoungApplication.class, args);
+	}
+
+}
