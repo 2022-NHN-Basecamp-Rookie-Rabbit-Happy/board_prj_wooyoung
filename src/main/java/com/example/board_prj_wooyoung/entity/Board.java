b436d52d3b8efbf9229f422a459bee69e11a9351
wooyoung <souljit2@gmail.com>
2022-01-12 20:59:20 +0900

    feat: Board에 lazy loading 적용 및 테스트

--- src/main/java/com/example/board_prj_wooyoung/entity/Board.java
+++ src/main/java/com/example/board_prj_wooyoung/entity/Board.java
@@ -1,6 +1,7 @@
 package com.example.board_prj_wooyoung.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,7 @@ public class Board {
 
     private String content;
 
-    @ManyToOne /* Board : Member = N : 1 */
+    @ManyToOne(fetch = FetchType.LAZY) /* 명시적으로 지연 로딩 지정 */
     private Member writer;
 
 }
--- src/test/java/com/example/board_prj_wooyoung/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/board_prj_wooyoung/repository/BoardRepositoryTest.java
@@ -6,6 +6,7 @@ import com.example.board_prj_wooyoung.entity.Board;
 import com.example.board_prj_wooyoung.entity.Member;
 import java.util.Optional;
 import java.util.stream.IntStream;
+import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,6 +37,7 @@ public class BoardRepositoryTest {
         });
     }
 
+    @Transactional /* 지연 로딩으로 설정했는데, writer를 불러오려 하니 세션이 끊어지는 error 발생. so, transactional 지정*/
     @Test
     public void testRead1() {
 
