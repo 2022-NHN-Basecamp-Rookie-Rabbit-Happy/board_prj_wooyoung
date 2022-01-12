7bdf0bc4b46473c7df89dc2ea5d1673036c42f9c
wooyoung <souljit2@gmail.com>
2022-01-12 20:12:56 +0900

    feat: Board - Member N:1 연관관계 mapping
    - @ManyToOne annotation을 사용하여 Board와 Member를 N:1 연관관계로 map

--- src/main/java/com/example/board_prj_wooyoung/entity/Board.java
+++ src/main/java/com/example/board_prj_wooyoung/entity/Board.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,5 +26,8 @@ public class Board {
     private String title;
 
     private String content;
+
+    @ManyToOne /* Board : Member = N : 1 */
+    private Member member;
 
 }
