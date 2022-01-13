bc1277f825ad8f12b15693ab83c9101d6f5e335f
wooyoung <souljit2@gmail.com>
2022-01-13 10:40:55 +0900

    feat: 게시글 검색 기능 및 테스트 구현

--- src/main/java/com/example/board_prj_wooyoung/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/com/example/board_prj_wooyoung/repository/search/SearchBoardRepositoryImpl.java
@@ -4,10 +4,20 @@ import com.example.board_prj_wooyoung.entity.Board;
 import com.example.board_prj_wooyoung.entity.QBoard;
 import com.example.board_prj_wooyoung.entity.QMember;
 import com.example.board_prj_wooyoung.entity.QReply;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.core.types.Order;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 @Log4j2
@@ -43,6 +53,78 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         log.info(result);
 
         return null;
+
+    }
+
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+
+        log.info("searchPage......................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression booleanExpression = board.bno.gt(0L);
+
+        booleanBuilder.and(booleanExpression);
+
+        if (type != null) {
+            String[] typeArr = type.split("");
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for (String t : typeArr) {
+                switch (t) {
+                    case "t":
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        Sort sort = pageable.getSort();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class, "board");
+            tuple.orderBy(new OrderSpecifier(direction, orderByExpression.get(prop)));
+        });
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        long count = tuple.fetchCount();
+
+        log.info("COUNT: " + count);
+
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+
+        return new PageImpl<Object[]>(
+            result.stream().map(t -> t.toArray()).collect(Collectors.toList()),
+            pageable,
+            count
+        );
 
     }
 }
