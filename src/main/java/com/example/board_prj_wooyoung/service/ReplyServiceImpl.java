67c087bdac304536cef8f4bae90a7bf2dffa10a9
wooyoung <souljit2@gmail.com>
2022-01-13 11:39:47 +0900

    feat: ReplyController에 GET Reply List API 추가

--- src/main/java/com/example/board_prj_wooyoung/service/ReplyServiceImpl.java
+++ src/main/java/com/example/board_prj_wooyoung/service/ReplyServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReplyServiceImpl implements ReplyService {
 
-    private ReplyRepository replyRepository;
+    private final ReplyRepository replyRepository;
 
     @Override
     public Long register(ReplyDTO replyDTO) {
